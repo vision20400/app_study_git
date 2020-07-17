f047381357048eaafe6dbf95029895ca4853d316
ll_sejin <41162249+vision20400@users.noreply.github.com>
2020-07-17 22:08:59 +0900

    에베베ㅔ

--- app/src/main/java/com/example/fragment/AFragment.java
+++ app/src/main/java/com/example/fragment/AFragment.java
@@ -16,7 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AFragment extends Fragment {
-
+//git test
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
